eadb8d2c095eed5e2e67ec35dae837cb92d50a31
mangmuse <mangmangmuse@gmail.com>
2024-06-01 23:34:51 +0900

    update supabase set up

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,12 +1,16 @@
+import useSignup from "../../hooks/useSignup";
+
 const Signup = ({ togglePage }) => {
+  const { email, setEmail, password, setPassword, error, loading, handleSignup } = useSignup();
+  console.log(email, password);
   return (
-    <>
+    <form onSubmit={handleSignup}>
       <h3>회원가입</h3>
-      <input type="email" />
-      <input type="password" />
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
       <button>ㅇㅇㅇ</button>
       <span onClick={togglePage}>로그인 어쩌구</span>
-    </>
+    </form>
   );
 };
 
